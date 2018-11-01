8f28cd762ee3512a32167ba9da2354bf4e57692b
Viktor Zimmermann <viktorzimmermann11@web.de>
2018-11-01 10:32:54 +0100

    added

--- src/components/signup/Pro.jsx
+++ src/components/signup/Pro.jsx
@@ -1,28 +1,68 @@
 import React from "react";
 
 class Pro extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            disabled: true,
+            email: '',
+            password: '',
+            password_again: ''
+
+        }
+
+    }
+
+    inputChanged = (event) => {
+        console.log(event.target.value);
+        let obj = {};
+        obj[event.target.name] = event.target.value;
+        this.setState(obj, this.checkValid);
+    };
+
+    checkValid = () => {
+        console.log(this.state.email, this.state.password, this.state.password_again);
+
+
+
+        this.setState({
+            disabled: false,
+        })
+    };
+
+
     render() {
         return (
             <div className="text-center">
                 Trial Component
                 <div>
                     <div>Email Adress</div>
-                    <input type="text" name="email"/>
+                    <input
+                        type="text" name="email"
+                        value={this.state.email}
+                        onChange={this.inputChanged.bind(this)}
+                    />
                 </div>
                 <div>
                     <div>Password</div>
-                    <input type="password" name="password"/>
+                    <input
+                        type="password"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.inputChanged.bind(this)}
+                    />
                 </div>
                 <div>
                     <div>Reapeat your password</div>
-                    <input type="passwowrd" name="password_again"/>
+                    <input
+                        type="passwowrd"
+                        name="password_again"
+                        value={this.state.password_again}
+                        onChange={this.inputChanged.bind(this)}
+                    />
                 </div>
                 <button onClick={this.props.onReset}>Back</button>
-                <button disabled>Submit</button>
+                <button disabled={this.state.disabled}>Submit</button>
 
             </div>
         );
