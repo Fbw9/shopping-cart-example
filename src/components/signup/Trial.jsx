94cc678b475bf52242267091340a93665564bb1a
Viktor Zimmermann <viktorzimmermann11@web.de>
2018-11-01 10:35:31 +0100

    chenged

--- src/components/signup/Trial.jsx
+++ src/components/signup/Trial.jsx
@@ -3,31 +3,40 @@ import React from "react";
 class Trial extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
             disabled: true,
             email: '',
             password: '',
-            password_again: ''
-
+            password_again: '',
         }
-
     }
 
-    inputChanged = (event) => {
-        console.log(event.target.value);
+    inputChanged = (e) => {
         let obj = {};
-        obj[event.target.name] = event.target.value;
+        obj[e.target.name] = e.target.value;
         this.setState(obj, this.checkValid);
-    };
+    }
 
-    checkValid = () => {
-        console.log(this.state.email, this.state.password, this.state.password_again);
+    checkValid = () =>  {
+        let disabled = true;
+        let passed = {
+            'email': false,
+            'password': true,
+            'password_again': true,
+        }
 
+        if(/^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9-]+[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/.test(this.state.email)) {
+            passed.email = true;
+        }
 
+        if (passed.email &&  passed.password && passed.password_again) {
+            disabled = false;
+        }
 
         this.setState({
-            disabled: false,
-        })
+            disabled: disabled,
+        });
     };
 
 
@@ -35,20 +44,22 @@ class Trial extends React.Component {
         return (
           <div className="text-center">
                 Trial Component
+                <hr/>
                 <div>
-                    <div>Email Adress</div>
+                    <div>Email Address</div>
                     <input type="text" name="email" value={this.state.email} onChange={this.inputChanged.bind(this)}/>
                 </div>
-              <div>
-                  <div>Password</div>
-                  <input type="password" name="password" value={this.state.password} onChange={this.inputChanged.bind(this)}/>
-              </div>
-              <div>
-                  <div>Reapeat your password</div>
-                  <input type="passwowrd" name="password_again" value={this.state.password_again} onChange={this.inputChanged.bind(this)}/>
-              </div>
-              <button onClick={this.props.onReset}>Back</button>
-              <button disabled={this.state.disabled}>Submit</button>
+                <div>
+                    <div>Password</div>
+                    <input type="password" name="password" value={this.state.password} onChange={this.inputChanged.bind(this)}/>
+                </div>
+                <div>
+                    <div>Repeat your password</div>
+                    <input type="password" name="password_again" value={this.state.password_again} onChange={this.inputChanged.bind(this)}/>
+                </div>
+                <hr/>
+                <button onClick={this.props.onReset}>Back</button>
+                <button disabled={this.state.disabled}>Submit</button>
 
           </div>
         );
